987a79f77b73a0ad40dff2c852282007cc55929b
Harish Kumar <harishkumar9758@gmail.com>
2021-06-02 10:50:48 +0530

    Commit

--- Sample/src/main/java/org/harish/springboot/TestApp.java
+++ Sample/src/main/java/org/harish/springboot/TestApp.java
@@ -1,16 +1,20 @@
 package org.harish.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+@Configuration
 @RestController
 public class TestApp {
 
-	@GetMapping("sayHello")
+	@GetMapping("hello")
 	public String sayHello() {
 		return "Hello world";
 	}
